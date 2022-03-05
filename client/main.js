dd80e035e10547b7b7c0ecda81d1cd1ec05e9ff6
Travis Gallagher <92351358+travisgallagher@users.noreply.github.com>
2022-03-04 17:13:37 -0700

    after review

---
+++ client/main.js
@@ -0,0 +1,180 @@
+//const res = require("express/lib/response");
+
+document.getElementById("complimentButton").onclick = function () {
+    axios.get("http://localhost:4000/api/compliment/")
+        .then(function (response) {
+        const data = response.data;
+        alert(data);
+        });
+};
+
+// const fortuneBtn = document.getElementById("fortuneButton").onclick = function () {
+//     axios.get("http://localhost:4000/api/fortune/")
+//         .then(function (response) {
+//         const data = response.data;
+//         alert(data);
+//         });
+// };
+
+
+// Step 1: Grab the element from HTML
+const fortuneBtn = document.getElementById("fortuneButton"); 
+
+const nameBtn = document.getElementById("nameButton");
+const nameInput = document.getElementById("nameInput");
+
+const indexBtn = document.getElementById("indexButton"); 
+const indexInput = document.getElementById("indexInput"); 
+
+const form = document.querySelector("form"); 
+const newNameInput = document.getElementById("newNameInput")
+const newNameIndex = document.getElementById("newNameIndex")
+const submitForm = document.getElementById(`submitForm`)
+
+
+// Step 2: Write out the functionality of the element
+function getFortune() {
+    axios.get("http://localhost:4000/api/fortune")
+        .then((res) => {
+            console.log(res.data); 
+            alert(res.data); 
+        });
+};
+
+function createName() {
+    const newName = nameInput.value; 
+
+    const body = {
+        newName // newName: newName
+    };
+
+    axios.post("http://localhost:4000/api/name", body)
+        .then((res) => {
+            // console.log(res.data);
+            let name = res.data[res.data.length - 1]; //grabs last element in array
+            alert(`you just registered ${name}`)
+            console.log(res.data)
+
+            nameInput.value = ``; 
+        }) 
+}
+
+function deleteName() {
+    const newIndex = indexInput.value; 
+
+    axios.delete(`http://localhost:4000/api/delete/${newIndex}`)
+        .then((res) => {
+            alert("You have successfully deleted a user. ")
+            console.log(res.data)
+        })
+        .catch((err) => {
+            console.log(err.response.data); 
+        })
+}
+
+function editName() {
+    const nameChange = newNameInput.value; 
+    const indexChange = newNameIndex.value; 
+
+    const body = {
+        nameChange 
+    };
+
+    axios.put(`http://localhost:4000/api/edit/${indexChange}`, body)
+        .then((res) => {
+            alert(`Your users array now contains ${res.data}`)
+            console.log(res.data)
+        })
+        .catch
+        console.log((err) => {
+            console.log(err.response.data); 
+        }); 
+
+}
+
+// Step 3: Add the event listener to my element. 
+// params for event listener: event and callback
+fortuneBtn.addEventListener("click", getFortune); 
+nameBtn.addEventListener("click", createName); 
+indexBtn.addEventListener("click", deleteName); 
+form.addEventListener("submit", editName); 
+
+
+
+
+
+
+
+
+//   const removeAllChildNodes = (parent) => {
+//     while (parent.firstChild) {
+//         parent.removeChild(parent.firstChild);
+//     }
+//   }
+
+// const editGoal = (id,goalDesc,goalText) => {
+//   const goalDescription = document.getElementById("goalDescription")
+//   const goal = document.getElementById("goal");
+//   goalDescription.value = goalDesc;
+//   goal.value = goalText;
+
+//   axios.put(`http://localhost:4000/api/goals/${id}`, {goalDescription:goalDescription.value,goal:goal.value}).then((res) => {
+//     populateGoals(res.data)
+//     console.log(res)
+//   }).catch(err => {
+//     console.error({err})
+//   })
+
+// }
+//   const populateGoals = (goals) => {
+//     const goalsContainer = document.getElementById("goals");
+//     if(goals.length === 0) {removeAllChildNodes(goalsContainer); return;};
+
+//     goals.forEach((goal,i) => {
+//       const goalContainer = document.createElement("div");
+//       const goalDesc = document.createElement("p");
+//       const goalText = document.createElement("p");
+//       const deleteButton = document.createElement("button");
+//       const editButton = document.createElement("button");
+//       goalDesc.innerHTML = `Desc: ${goal.goalDescription}`;
+//       goalText.innerHTML = `Goal: ${goal.goal}`;
+//       deleteButton.onclick = () => {
+//         axios.delete(`http://localhost:4000/api/goals/${i}`).then(res => {
+//           console.log(res);
+//           populateGoals([])
+//         }).catch(err => {
+//           console.log({err});
+//         }) 
+//       }
+//       editButton.onclick = (e) => {
+//         e.preventDefault();
+//         editGoal(i,goalDesc.innerHTML,goalText.innerHTML)};
+//       goalContainer.style.cursor = 'pointer'
+//       deleteButton.innerText = "Delete";
+//       editButton.innerText = "Edit";
+//       goalContainer.append(goalDesc,goalText,deleteButton,editButton);
+//       goalsContainer.append(goalContainer);
+//     });
+//   }
+
+//   function getAllGoals() {
+//     axios.get("http://localhost:4000/api/goals")
+//       .then(function(res) {
+//         const data = response.data; 
+//         alert(data);  
+//       })
+
+//   }
+//   document.getElementById("addGoalButton").onclick = function (e) {
+//     e.preventDefault();
+//     const goalDescription = document.getElementById("goalDescription").value;
+//     const goal = document.getElementById("goal").value; 
+//     const body = { goalDescription, goal };
+//     axios.post("http://localhost:4000/api/goals/", body)
+//         .then((response)  => {
+//           const data = response.data;
+//           populateGoals(data)
+//         }).catch(error => {
+//           console.log({error})
+//         });
+//   };
--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const cors = require("cors");
+const res = require("express/lib/response");
 
 const app = express();
 
+const users = []; 
 
 const goals = []; 
 let globalID = 0; 
@@ -13,8 +15,8 @@ app.use(express.json()); // When we want to be able to accept JSON.
 
 app.get("/api/compliment", (req, res) => {
   const compliments = ["Gee, you're a smart cookie!",
-					"Cool shirt!",
-					"Your Javascript skills are stellar.",
+    "Cool shirt!",
+    "Your Javascript skills are stellar.",
   ];
 
   // choose random compliment
@@ -26,15 +28,15 @@ app.get("/api/compliment", (req, res) => {
 
 app.get("/api/fortune", (req, res) => {
   const fortunes = [
-          "A beautiful, smart, and loving person will be coming into your life.",
-					"A dubious friend may be an enemy in camouflage.",
-					"A faithful friend is a strong defense.",
-          "A feather in the hand is better than a bird in the air.",
-          "A fresh start will put you on your way.", 
-          "A friend asks only for your time not your money.", 
-          "A good time to finish up old tasks.",
-          "A lifetime friend shall soon be made.",
-  ];
+    "A beautiful, smart, and loving person will be coming into your life.",
+    "A dubious friend may be an enemy in camouflage.",
+    "A faithful friend is a strong defense.",
+    "A feather in the hand is better than a bird in the air.",
+    "A fresh start will put you on your way.", 
+    "A friend asks only for your time not your money.", 
+    "A good time to finish up old tasks.",
+    "A lifetime friend shall soon be made.",
+];
   // choose random compliment
   let randomIndexFortune = Math.floor(Math.random() * fortunes.length);
   let randomFortune = fortunes[randomIndexFortune];
@@ -42,35 +44,66 @@ app.get("/api/fortune", (req, res) => {
   res.status(200).send(randomFortune);
 });
 
-let id = 0
+app.post("/api/name", (req, res) => {
+    console.log(req.body);
+    console.log(req.params);
+    console.log(req.query); 
 
-app.post("/api/goals", (req, res) => {
-  let newGoal = {...req.body, id}
-  newGoal.id = globalID; 
-  goals.push(newGoal); 
-  res.status(200).send(goals);
-  globalID++;  
-})
+    const {newName} = req.body; 
 
-app.delete("/api/goals/:id", (req, res) => {
-  let index = goals.findIndex(goal => goal.id === +req.params.id); 
-  goals.splice(index, 1); 
-  res.status(200).send(goals); 
+    users.push(newName); 
+    res.status(200).send(users); 
+
+
 });
 
-app.put("/api/goals/:id", (req, res) => {
-  let {id} = req.params; 
-  let body= req.body; 
-  let index = goals.findIndex(goal => +goal.id === +id); 
-console.log(id,body,index)
-  goals[index].goalDescription = body.goalDescription;
-  goals[index].goals = body.goal;
-  res.status(200).send(goals);
-})
+app.delete("/api/delete/:index", (req, res) => {
+  console.log(req.params); 
 
+  if(+req.params.index) {
+    users.splice(req.params.index, 1); 
+    res.status(200).send(users); 
+  } else {
+    res.status(400).send("Dont have number. ")
+    //console.log("Dont have a number.")
+  }
+});
 
+app.put("/api/edit/:id", (req, res) => {
+  console.log(req.params);
+  console.log(req.body);
 
+  const {nameChange} = req.body; 
+  const editIndex = +req.params.id; 
 
+  users[editIndex] = nameChange; 
 
+  res.status(200).send(users); 
+});
+
+let id = 0
+// app.post("/api/goals", (req, res) => {
+//   let newGoal = {...req.body, id}
+//   newGoal.id = globalID; 
+//   goals.push(newGoal); 
+//   res.status(200).send(goals);
+//   globalID++;  
+// })
+
+// app.delete("/api/goals/:id", (req, res) => {
+//   let index = goals.findIndex(goal => goal.id === +req.params.id); 
+//   goals.splice(index, 1); 
+//   res.status(200).send(goals); 
+// });
+
+// app.put("/api/goals/:id", (req, res) => {
+//   let {id} = req.params; 
+//   let body= req.body; 
+//   let index = goals.findIndex(goal => +goal.id === +id); 
+// console.log(id,body,index)
+//   goals[index].goalDescription = body.goalDescription;
+//   goals[index].goals = body.goal;
+//   res.status(200).send(goals);
+// })
 
 app.listen(4000, () => console.log("Server running on 4000"));
